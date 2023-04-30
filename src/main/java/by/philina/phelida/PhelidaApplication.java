d91141f57e09e2a352604f153be05e16718f2735
Philina-vn <filina26112002@gmail.com>
2023-05-01 01:42:16 +0300

    feat: init

---
+++ src/main/java/by/philina/phelida/PhelidaApplication.java
@@ -0,0 +1,13 @@
+package by.philina.phelida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhelidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhelidaApplication.class, args);
+    }
+
+}
